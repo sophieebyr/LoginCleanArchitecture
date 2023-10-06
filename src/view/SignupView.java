46263b9d9453998a91aaef1d3919b49b00a4f6b7
sophierohrer <sophie.rohrer@mail.utoronto.ca>
2023-10-06 15:39:11 -0400

    finished lab tasks

--- src/view/SignupView.java
+++ src/view/SignupView.java
@@ -26,6 +26,10 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
     private final JButton cancel;
 
     public SignupView(SignupController controller, SignupViewModel signupViewModel) {
+        // Step 3 Q1
+        // name - signupViewModel
+        // initialized - below in this.signupViewModel = signupViewModel
+        // class that instantiates SignUpView - SignUpView
 
         this.signupController = controller;
         this.signupViewModel = signupViewModel;
@@ -51,10 +55,27 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
                 // This creates an anonymous subclass of ActionListener and instantiates it.
                 new ActionListener() {
                     public void actionPerformed(ActionEvent evt) {
+                        // This is the method that gets called when an action event happens,
+                        // like clicking a button
+                        // The 'ActionEvent evt' parameter contains information about the event.
                         if (evt.getSource().equals(signUp)) {
-                            signupController.execute(usernameInputField.getText(),
-                                    String.valueOf(passwordInputField.getPassword()),
-                                    String.valueOf(repeatPasswordInputField.getPassword()));
+                            SignupState currentState = signupViewModel.getState();
+                            String username = currentState.getUsername();
+                            String password = currentState.getPassword();
+                            String repeatPassword = currentState.getRepeatPassword();
+                            signupController.execute(username, password, repeatPassword);
+                            // this line checks if the source of the event was the 'signUp' object.
+                            // If Sign-Up was clicked.
+                            // If the above condition is true (ie, if signUp was clicked),
+                            // this line calls a method named "execute" from "signupController".
+//                            signupController.execute(usernameInputField.getText(),
+//                                    // retrieves whatever text has been typed into a field named
+//                                    // "usernameInputField".
+//                                    String.valueOf(passwordInputField.getPassword()),
+//                                    String.valueOf(repeatPasswordInputField.getPassword()));
+                                    // lines retrieve whatever has been typed into fields
+                            // named "passwordInputField" and "repeatPasswordInputField",
+                            // respectively, and convert them to strings using String.valueOf().
                         }
                     }
                 }
@@ -67,11 +88,18 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         // Notice how it has access to instance variables in the enclosing class!
         usernameInputField.addKeyListener(
                 new KeyListener() {
+                    // anonymous subclass of Key Listener and instantiates
                     @Override
                     public void keyTyped(KeyEvent e) {
                         SignupState currentState = signupViewModel.getState();
+                        // retrieves current state of sign-up process
                         currentState.setUsername(usernameInputField.getText() + e.getKeyChar());
+                        // sets username in that state object with current
+                        // text present in input field plus character just typed
+                        // e.getKeyChar() returns character corresponding to key just typed.
+                        // So essentially it updates username in state as user types
                         signupViewModel.setState(currentState);
+                        // it sets updated state back into view model
                     }
 
                     @Override
@@ -82,6 +110,42 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
                     public void keyReleased(KeyEvent e) {
                     }
                 });
+        // Add KeyListener for passwordInputField
+        passwordInputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                SignupState currentState = signupViewModel.getState();
+                currentState.setPassword(String.valueOf(passwordInputField.getPassword()) + e.getKeyChar());
+                signupViewModel.setState(currentState);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+// Add KeyListener for repeatPasswordInputField
+        repeatPasswordInputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                SignupState currentState = signupViewModel.getState();
+                currentState.setRepeatPassword(String.valueOf(repeatPasswordInputField.getPassword()) + e.getKeyChar());
+                signupViewModel.setState(currentState);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
